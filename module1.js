c5d52ac51e617812f75bd75e87b57c51b83b2a1d
Gerardo Montero <gerardomontme@gmail.com>
2021-07-26 15:12:51 -0700

    first_commit

---
+++ module1.js
@@ -0,0 +1,8 @@
+const styling = {
+  font: 'Monaco',
+  color1: 'black',
+  color2: 'grey',
+  bgColor: 'white'
+}
+
+exports.styling = styling;
---
+++ server.js
@@ -0,0 +1,69 @@
+
+const module1 = require('./module1');
+const styles = module1.styling;
+const path = require('path');
+const http = require('http');
+const formidable = require('formidable');
+const fs = require('fs');
+
+const directoryPath = path.join(__dirname, 'images');
+let imgArr = [];
+http.createServer(function (req, res) {
+    switch (req.url) {
+        case '/fileupload':
+            const form = new formidable.IncomingForm();
+            form.parse(req, function (err, fields, files) {
+              const nameOfFile = `image${imgArr.length+1}.jpg`
+              const oldpath = files.filetoupload.path;
+              const newpath = './images/' + nameOfFile;
+              fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.end(`
+                    <div style="text-align:center; font-family:${styles.font}">
+                     <p>Image uploaded successfully<p><br/>
+                     <a style="color:${styles.color2}" href="./images">See images</a><br/>
+                     <a style="color:${styles.color2}" href="/">Return</a>
+                    </div>`
+                    );
+                });
+            });
+            break;
+        case '/images':
+            imgArr=[];
+            fs.readdir(directoryPath, function (err, files) {
+                if (err) {
+                    return console.log('Unable to scan directory: ' + err);
+                } 
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(`<ul style="font-family:${styles.font}">`);
+                files.forEach(function (file) {
+                    imgArr.push(file);
+                    res.write(`<li><a style="color:${styles.color1}" href="./images/${file}">${file}</a></li>`);
+                });
+                res.write(`</ul>`);
+                res.write(`<a style="color:${styles.color2}" href="/">Return</a>`);
+                res.end();
+            });
+            break;
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(`
+            <div style="margin:auto; text-align:center; font-family:${styles.font}">
+                <h1>Image Storage</h1><br/>
+                <form style="border:1px solid black; width:fit-content; padding:10px; margin:auto" action="fileupload" method="post" enctype="multipart/form-data">
+                    <input type="file" name="filetoupload"><br/><br/>
+                    <input type="submit">
+                </form><br/>
+                <a style="color:${styles.color2}" href="./images">See images</a>
+            </div>`);
+            res.end();
+            break;
+    }
+    for(let image of imgArr) {
+        if (req.url == `/images/${image}` ) {
+            res.writeHead(200,{'content-type':'image/jpg'});
+            fs.createReadStream(`./images/${image}`).pipe(res);
+            break;
+        }
+    }
+}).listen(8080);
